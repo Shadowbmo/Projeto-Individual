f0d3415bd61f36b64279eb0831e34173ab92f5f1
Shadowbmo <douglas.silva@sptech.school>
2022-11-25 07:18:33 -0300

    Colocado a Api para funcionar feito as validação, feito e feito o carrosel com vetor

---
+++ site/public/js/app.js
@@ -0,0 +1,16 @@
+
+var botaoLogin = document.querySelector("#login");
+var botaoCadastro = document.querySelector("#cadastro");
+
+var body = document.querySelector("body");
+
+
+btnSignin.addEventListener("click", function () {
+    containerPadrao.className = "sign-in-js"; 
+});
+
+btnSignup.addEventListener("click", function () {
+    containerPadrao.className = "sign-up-js";
+})
+
+
